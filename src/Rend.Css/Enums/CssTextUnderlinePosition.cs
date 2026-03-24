e4d2dc1a1cdf7d87482b214d3577d322cf9cbd2b
DevNamedZed <DevNamedZed@gmail.com>
2026-03-24 03:11:10 -0700

    add missing file

---
+++ src/Rend.Css/Enums/CssTextUnderlinePosition.cs
@@ -0,0 +1,15 @@
+namespace Rend.Css
+{
+    /// <summary>
+    /// CSS text-underline-position property values.
+    /// <spec>CSS-TEXT-DECOR-4 §3.3 https://drafts.csswg.org/css-text-decor-4/#text-underline-position-property</spec>
+    /// </summary>
+    public enum CssTextUnderlinePosition : byte
+    {
+        Auto,
+        Under,
+        FromFont,
+        Left,
+        Right
+    }
+}
